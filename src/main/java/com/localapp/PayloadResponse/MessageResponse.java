ee9428ef78edb8d1156b90b1314d4596672114c9
ShraddhaDurge <shraddhadurge123@gmail.com>
2022-01-23 22:27:34 +0530

    Login and Registration

---
+++ src/main/java/com/localapp/PayloadResponse/MessageResponse.java
@@ -0,0 +1,24 @@
+package com.localapp.PayloadResponse;
+
+public class MessageResponse {
+	private String message;
+
+	public MessageResponse(String message) {
+	    this.message = message;
+	  }
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "MessageResponse{" +
+				"message='" + message + '\'' +
+				'}';
+	}
+}
